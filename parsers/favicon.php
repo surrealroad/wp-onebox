25d7f6af3a2e6c1d1e9273d72d68d81bfa6aafa8
Jack James <jack@surrealroad.com>
2013-12-10 16:26:07 +0000

    coerce favicon to string

--- parsers/favicon.php
+++ parsers/favicon.php
@@ -9,21 +9,22 @@ if(isset($onebox)) {
 
 	if($match) {
 		$data =  get_favicon($onebox->getDoc());
-		$onebox->update($data);
+		if($data) $onebox->update($data);
 	}
 }
 
 
 function get_favicon($doc) {
 	// http://stackoverflow.com/questions/5701593/how-to-get-a-websites-favicon-with-php
+	$data = array();
 	$xpath = new DOMXPath($doc);
 	$xml = simplexml_import_dom($doc);
 	if($xml) {
 		$arr = $xml->xpath('//link[@rel="shortcut icon"]');
-		if(isset($arr[0]['href'])) $data['favicon'] = $arr[0]['href'];
+		if(isset($arr[0]['href'])) $data['favicon'] = (string)$arr[0]['href'];
 		else {
 			$arr = $xml->xpath('//link[@rel="icon"]');
-			if(isset($arr[0]['href'])) $data['favicon'] = $arr[0]['href'];
+			if(isset($arr[0]['href'])) $data['favicon'] = (string)$arr[0]['href'];
 		}
 	}
 	return $data;
