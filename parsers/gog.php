3f4b334aa9869762ca5b4306f677d28d1e43780f
Jack James <jack@surrealroad.com>
2013-12-13 00:01:00 +0000

    gog genres, error masking

--- parsers/gog.php
+++ parsers/gog.php
@@ -39,29 +39,31 @@ function get_gog_data($url) {
 		//$data['image']= $img->item(0)->getAttribute("src");
 
 		$additional = array();
-		$genrelist = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' game_top ')]/ul[contains(concat(' ', normalize-space(@class), ' '), ' details ')]/li[0]/a");
-		if($genrelist) {
+		@$genrelist = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' game_top ')]/ul[contains(concat(' ', normalize-space(@class), ' '), ' details ')]/li[1]/a");
+		if(count($genrelist)) {
 			$genres = array();
-			foreach($genrelist->nodeValue as $genre) {
-				print_r($genre);
-				$genres[]=$genre;
+			foreach($genrelist as $genre) {
+				$genres[]=$genre->nodeValue;
 			}
 			$additional[]= __('Genre: ', "onebox").implode(", ", $genres);
 		}
 
-		$fullrating = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' game_top ')]/ul/li/span[contains(concat(' ', normalize-space(@class), ' '), ' usr_rate ')]/span[contains(concat(' ', normalize-space(@class), ' '), ' usr_s_f ')]");
-		$halfrating = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' game_top ')]/ul/li/span[contains(concat(' ', normalize-space(@class), ' '), ' usr_rate ')]/span[contains(concat(' ', normalize-space(@class), ' '), ' usr_s_h ')]");
-		$rating = $fullrating->length + 0.5 * $halfrating->length;
-		$ratingCountText = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' game_top ')]/ul/li/span[contains(concat(' ', normalize-space(@class), ' '), ' usr_rate ')]/../span")->item(1)->nodeValue;
+		@$fullrating = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' game_top ')]/ul/li/span[contains(concat(' ', normalize-space(@class), ' '), ' usr_rate ')]/span[contains(concat(' ', normalize-space(@class), ' '), ' usr_s_f ')]");
+		@$halfrating = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' game_top ')]/ul/li/span[contains(concat(' ', normalize-space(@class), ' '), ' usr_rate ')]/span[contains(concat(' ', normalize-space(@class), ' '), ' usr_s_h ')]");
+		@$rating = $fullrating->length + 0.5 * $halfrating->length;
+		@$ratingCountText = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' game_top ')]/ul/li/span[contains(concat(' ', normalize-space(@class), ' '), ' usr_rate ')]/../span")->item(1)->nodeValue;
 		preg_match_all('/\d+/', $ratingCountText, $matches); // http://stackoverflow.com/questions/11243447/get-numbers-from-string-with-php
-		$ratingCount = $matches[0][0];
+		@$ratingCount = $matches[0][0];
 		$data['titlebutton']= '<div class="onebox-rating"><span class="onebox-stars">'.$rating.'</span> ('.intval($ratingCount).')</div>';
 
-		$releaseDate = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' game_top ')]/ul[contains(concat(' ', normalize-space(@class), ' '), ' details ')]/li")->item(3)->nodeValue;
-		if($releaseDate) $data['footer']= __('Released: ', "onebox").'<strong>'.$releaseDate.'</strong>';
+		$footer = array();
+		@$releaseDate = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' game_top ')]/ul[contains(concat(' ', normalize-space(@class), ' '), ' details ')]/li")->item(3)->nodeValue;
+		if($releaseDate) $footer[]= __('Released: ', "onebox").'<strong>'.$releaseDate.'</strong>';
+		@$size = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' download_size ')]/b")->item(0)->nodeValue;
+		if($size) $footer[]= __('Download size: ', "onebox").'<strong>'.$size.'</strong>';
 
 
-		$title = $finder->query('//title')->item(0)->nodeValue;
+		@$title = $finder->query('//title')->item(0)->nodeValue;
 		$regs = array();
 		if (preg_match('/(?<=\$)\d+(\.\d+)?\b/', $title, $regs)) {
 	    	$data['footerbutton']= '<a href="'.$data['displayurl'].'">$'.$regs[0].'</a>';
@@ -69,6 +71,9 @@ function get_gog_data($url) {
 
 		if(count($additional)) {
 			$data['additional'] = implode("<br/>", $additional);
+		}
+		if(count($footer)) {
+			$data['footer'] = implode(" &middot; ", $footer);
 		}
 	}
 	return $data;
