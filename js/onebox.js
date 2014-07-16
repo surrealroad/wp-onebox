b818cbaef9727ebe3b47073eab108ee21e0177ec
Jack James <sales@surrealroad.com>
2014-07-16 15:50:31 +0100

    make plugin chainable

--- js/onebox.js
+++ js/onebox.js
@@ -59,6 +59,7 @@
 				});
 			});
 		}
+		return this;
 	};
 })(jQuery);
 
@@ -67,17 +68,18 @@
 (function($) {
 	jQuery.fn.oneboxstars = function() {
 		if ($(this).exists()) {
-		return $(this).each(function() {
-			// Get the value
-			var val = parseFloat($(this).html());
-			// Make sure that the value is in 0 - 5 range, multiply to get width
-			var size = Math.max(0, (Math.min(5, val))) * 16;
-			// Create stars holder
-			var $span = $('<span />').width(size);
-			// Replace the numerical value with stars
-			$(this).html($span);
-		});
-	}
+			return $(this).each(function() {
+				// Get the value
+				var val = parseFloat($(this).html());
+				// Make sure that the value is in 0 - 5 range, multiply to get width
+				var size = Math.max(0, (Math.min(5, val))) * 16;
+				// Create stars holder
+				var $span = $('<span />').width(size);
+				// Replace the numerical value with stars
+				$(this).html($span);
+			});
+		}
+		return this;
 	};
 })(jQuery);
 
