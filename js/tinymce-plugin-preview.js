8440f212a3be1305a76080d22cd5316b973faf68
Jack James <sales@surrealroad.com>
2014-07-17 13:34:06 +0100

    just encode/decode shortcode text as-is

--- js/tinymce-plugin-preview.js
+++ js/tinymce-plugin-preview.js
@@ -10,16 +10,15 @@ tinymce.PluginManager.add( 'oneboxPreview', function( editor ) {
 	function fromShortcode( content ) {
 		// http://regexr.com/395l4
 		// http://stackoverflow.com/questions/24769465/how-can-i-use-regex-to-match-a-string-without-double-characters
-		return content.replace(/\[(?!\[)onebox\ .*url="(.*)"\](?!\])/g, function(a, url) {
-			return '<div class="onebox-container render mceNonEditable" data-url="'+url+'" contenteditable="false"><a href="'+url+'">Link</a><!--onebox-container-end--></div>';
+		return content.replace(/\[(?!\[)onebox\ .*url="(.*)"\](?!\])/g, function(shortcode, url) {
+			return '<div class="onebox-container render mceNonEditable" data-shortcode="'+window.encodeURIComponent(shortcode)+'" contenteditable="false"><a href="'+url+'">Link</a><!--onebox-container-end--></div>';
 		});
 	}
 
 	function toShortcode( content ) {
 		// http://regexr.com/395ov
-		return content.replace(/<div class="onebox-container(?:.*?)data-url="(.*?)"(.*?)<!--onebox-container-end--><\/div>/g, function(a, url) {
-			var options="";
-			return '[onebox url="'+url+'"'+options+']';
+		return content.replace(/<div class="onebox-container(?:.*?)data-shortcode="(.*?)"(.*?)<!--onebox-container-end--><\/div>/g, function(container, shortcode) {
+			return window.decodeURIComponent(shortcode);
 		});
 	}
 
