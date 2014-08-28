613374411215e942114861a6e3de1c0146f63ec7
Jack James <sales@surrealroad.com>
2014-08-28 12:41:39 +0100

    prefer PQ over open graph module, which is kind of unreliable

--- parsers/opengraph.php
+++ parsers/opengraph.php
@@ -4,12 +4,19 @@
 
 if(isset($onebox)) {
 	$properties = $onebox->availableProperties();
-
 	$match = !in_array("title", $properties) || !in_array("description", $properties) || !in_array("image", $properties) || !in_array("sitename", $properties);
 
 	if($match) {
-		$data =  get_opengraph_data($onebox->getDoc());
+		$data =  pq_get_opengraph_data($onebox->getHTML());
 		$onebox->update($data);
+
+		// verify we succeeded with this method, otherwise try with pq
+		$properties = $onebox->availableProperties();
+		$match = !in_array("title", $properties) || !in_array("description", $properties) || !in_array("image", $properties) || !in_array("sitename", $properties);
+		if($match) {
+			$data =  get_opengraph_data($onebox->getDoc());
+			$onebox->update($data);
+		}
 	}
 }
 
@@ -30,6 +37,29 @@ function get_opengraph_data($doc) {
 		@$data['imageheight']=$graph->image->height;
 	}*/
 	$data['sitename']=$graph->site_name;
+
+	return $data;
+}
+
+function pq_get_opengraph_data($html) {
+	$data = array();
+	phpQuery::newDocument($html);
+	$imageProp = pq("meta[property=og:image]")->attr("content");
+	$imageName = pq("meta[name=og:image]")->attr("content");
+	$titleProp = pq("meta[property=og:title]")->attr("content");
+	$titleName = pq("meta[name=og:title]")->attr("content");
+	$descProp = pq("meta[property=og:description]")->attr("content");
+	$descName = pq("meta[name=og:description]")->attr("content");
+	$siteProp = pq("meta[property=og:site_name]")->attr("content");
+	$siteName = pq("meta[name=og:site_name]")->attr("content");
+	if($imageProp) $data['image'] = $imageProp;
+	elseif($imageName) $data['image'] = $imageName;
+	if($titleProp) $data['title'] = $titleProp;
+	elseif($titleName) $data['title'] = $titleName;
+	if($descProp) $data['description'] = $descProp;
+	elseif($descName) $data['description'] = $descName;
+	if($siteProp) $data['sitename'] = $siteProp;
+	elseif($siteName) $data['sitename'] = $siteName;
 
 	return $data;
 }
